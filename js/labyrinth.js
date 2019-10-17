8b9b3dfb23c7c75421c7fa2530b83821c5b53512
Hantasmagoria <30933301+Hantasmagoria@users.noreply.github.com>
2019-10-17 16:28:02 +0800

    built further upon wireframe: -added engine -player -world -declassified main function due to containment issues -reworked resize method in view class -fixed the readme

--- js/labyrinth.js
+++ js/labyrinth.js
@@ -3,14 +3,31 @@
 // console.log("Entered the labyrinth.");
 
 class Labyrinth {
-  constructor() {
-    this.controller = new Controller();
-    this.view       = new View(document.getElementById('gameArea'));
-    this.game       = new Game();
-    this.gear       = new Gear();
-  }
   static link(){
-    Gear.rev();
-    // Controller.openEars();
+    setTimeout(()=>{document.querySelector('h1').style.display = 'none';rev();}, 1000);
   }
+  static resize(e){
+    view.resize(game.world.height/game.world.width)
+  }
+  static update(){
+    game.mov();
+  }
+  static render(){
+    view.visualize();
+  }
+}
+
+const rev = ()=>{
+  View.createCanvas();
+    controller = new Controller();
+    view       = new View(document.getElementById('gameArea'));
+    game       = new Game();
+    gear       = new Gear(60);
+   view.buffer.canvas.width =  game.world.width;
+   view.buffer.canvas.height =  game.world.height;
+   controller.openEars();
+   view.openEyes();
+   view.resize( game.world.height/ game.world.width);
+   Labyrinth.render();
+   gear.start();
 }
--- js/view.js
+++ js/view.js
@@ -9,21 +9,47 @@ class View {
     this.buffer  = document.createElement("canvas").getContext("2d"),
     this.context = gameArea.getContext("2d");
   }
+  hitBox(x,y,width,height,fill) {
+    this.buffer.fillStyle = fill;
+    this.buffer.fillRect(Math.floor(x),Math.floor(y),width,height);
+  }
+  backgroundColor(color) {
+    this.buffer.fillStyle = color;
+    this.buffer.fillRect(0,0,this.buffer.canvas.width,this.buffer.canvas.height);
+  }
+  visualize() {
+    this.backgroundColor( game.world.bgColor);
+    this.hitBox( game.world.player.posX, game.world.player.posY, game.world.player.width, game.world.player.height, game.world.player.color);
+    this.display();
+  }
   display() {
     this.context.drawImage(this.buffer.canvas, 0, 0, this.buffer.canvas.width, this.buffer.canvas.height, 0, 0, this.context.canvas.width, this.context.canvas.height)
   }
-  resize() {
-    console.log(`${this.context.canvas.width}x${this.context.canvas.height}`);
+  resize(aspect) {
+    console.log(`${this.context.canvas.width}x${this.context.canvas.height}`,);
     // console.log(document.documentElement.clientHeight,document.documentElement.clientWidth);
-    this.context.canvas.width = document.documentElement.clientWidth - 32;
-    this.context.canvas.height = document.documentElement.clientHeight - 32;
+    if (((document.documentElement.clientHeight-32)/(document.documentElement.clientWidth-32) > aspect)?true:false) {
+      this.context.canvas.width = (document.documentElement.clientWidth-32);
+      this.context.canvas.height = (document.documentElement.clientWidth-32)*(aspect);
+    }else {
+      this.context.canvas.width = (document.documentElement.clientHeight-32)/(aspect);
+      this.context.canvas.height = (document.documentElement.clientHeight-32);
+    }
     this.display();
   }
+  //// OLD resize method
+  // resize() {
+  //   console.log(`${this.context.canvas.width}x${this.context.canvas.height}`);
+  //   // console.log(document.documentElement.clientHeight,document.documentElement.clientWidth);
+  //   this.context.canvas.width = document.documentElement.clientWidth - 32;
+  //   this.context.canvas.height = document.documentElement.clientHeight - 32;
+  //   this.display();
+  // }
   static createCanvas(){
     document.getElementsByTagName('main')[0].innerHTML = `<canvas id="gameArea"></canvas>`;
     // console.log("created canvas.");
   }
   openEyes(){
-    window.addEventListener('resize',(e)=>{this.resize(e)});
+    window.addEventListener('resize',(e)=>{ Labyrinth.resize(e)});
   }
 }
